3a579a4fd342ee7ab622a84b8d47033d8fdb8304
Jarek Šedý <jareksedy@icloud.com>
2022-03-26 09:23:51 +0600

    Added actions, gesture recognizers etc.

---
+++ YSGBCoordinator/LoginViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  YSGBCoordinator
+//
+//  Created by Ярослав on 24.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    // MARK: -- Outlets.
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    private func setupGestures() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+            view.addGestureRecognizer(tap)
+    }
+    
+    private func registerNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    // MARK: -- Selectors.
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        
+        var keyboardFrame: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        var contentInset: UIEdgeInsets = self.scrollView.contentInset
+        
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        
+        contentInset.bottom = keyboardFrame.size.height + 50
+        scrollView.contentInset = contentInset
+    }
+
+    @objc func keyboardWillHide(notification:NSNotification) {
+        let contentInset: UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // MARK: -- Actions.
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+    }
+    
+    // MARK: -- Lifecycle.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupGestures()
+        registerNotifications()
+    }
+}
+
